a99f5f32be089f161f0359c8b831218b7a3e12c1
vslayer34 <mohamd.elnagar.34@gmail.com>
2024-02-03 19:44:16 +0200

    Converted Screen touch position to the world space

--- Assets/Scripts/BallHandler.cs
+++ Assets/Scripts/BallHandler.cs
@@ -5,15 +5,24 @@ using UnityEngine.InputSystem;
 
 public class BallHandler : MonoBehaviour
 {
+    private Camera _mainCamera;
     private Vector2 _touchPosition;
+    private Vector3 _worldTouchPosition;
 
+    private void Start()
+    {
+        _mainCamera = Camera.main;
+    }
 
     private void Update()
     {
         if (Touchscreen.current.primaryTouch.press.isPressed)
         {
             _touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
-            Debug.Log(_touchPosition);
+            _worldTouchPosition = _mainCamera.ScreenToWorldPoint(_touchPosition);
+            _worldTouchPosition.z = 0.0f;
+            
+            Debug.Log(_worldTouchPosition);
         }
     }
 }
