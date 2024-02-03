e8fe9f57d0d4ad7ae189adf9f3b8525a672197df
vslayer34 <mohamd.elnagar.34@gmail.com>
2024-02-03 20:00:12 +0200

    added simple control to the ball to fling it

--- Assets/Scripts/BallHandler.cs
+++ Assets/Scripts/BallHandler.cs
@@ -9,6 +9,10 @@ public class BallHandler : MonoBehaviour
     private Vector2 _touchPosition;
     private Vector3 _worldTouchPosition;
 
+
+    [SerializeField, Tooltip("Reference to the bauder rigid body")]
+    private Rigidbody2D _baulderRb;
+
     private void Start()
     {
         _mainCamera = Camera.main;
@@ -19,10 +23,18 @@ public class BallHandler : MonoBehaviour
         if (Touchscreen.current.primaryTouch.press.isPressed)
         {
             _touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
+            
             _worldTouchPosition = _mainCamera.ScreenToWorldPoint(_touchPosition);
             _worldTouchPosition.z = 0.0f;
+
+            _baulderRb.isKinematic = true;
+            _baulderRb.position = _worldTouchPosition;
             
             Debug.Log(_worldTouchPosition);
+        }
+        else
+        {
+            _baulderRb.isKinematic = false;
         }
     }
 }
